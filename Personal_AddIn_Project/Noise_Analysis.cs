33a7a8efa2a101ca8679ac201e6e99b8c035b632
Jonghun96 <kjh11538@naver.com>
2025-08-24 22:56:05 +0900

    Debug Connecting

---
+++ Personal_AddIn_Project/Noise_Analysis.cs
@@ -0,0 +1,19 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personal_AddIn_Project
+{
+    public class Noise_Analysis : IExternalCommand
+    {
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
